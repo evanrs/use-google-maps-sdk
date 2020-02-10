c7fef4f74f8228126124c6b1e6e0884b97727a92
evanrs <me@evanrs.com>
2020-02-10 13:35:25 -0500

    chore: export usePlacePredictions

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from "./use-script";
 export * from "./use-google-maps-sdk";
 export * from "./use-places-autocomplete-service";
 export * from "./use-places-autocomplete-session-token";
+export * from "./use-place-predictions";
